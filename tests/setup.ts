8fedbffc64314b7c5b3d39d179337ca644d67033
Anthony Penna <penna1296@gmail.com>
2021-12-12 10:36:50 +0100

    feat: pipeline

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,4 @@
-process.env.DB_USERNAME = 'username'
-process.env.DB_PASSWORD = 'password'
+process.env.MONGO_INITDB_ROOT_USERNAME = 'username'
+process.env.MONGO_INITDB_ROOT_PASSWORD = 'password'
 process.env.DB_HOST = 'host'
 process.env.DB_PORT = 'port'
